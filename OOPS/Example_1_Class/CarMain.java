fd2722d4b0db891f50a1b5df23747fda8a2f6bfa
Sonu Kumar <sonukumarraj007@gmail.com>
2025-04-27 10:44:20 +0530

    OOPS

---
+++ OOPS/Example_1_Class/CarMain.java
@@ -0,0 +1,28 @@
+package OOPS.Example_1_Class;
+
+public class CarMain {
+
+    public static void main(String[] args) {
+
+        // Creating an object of the Car class
+        Car myCar = new Car();
+
+        // Setting object values
+        myCar.color = "Red";
+        myCar.model = "Honda City";
+        myCar.year = 2022;
+
+        // Using object methods
+        myCar.startEngine();
+        myCar.showDetails();
+
+        // Creating another object
+        Car secondCar = new Car();
+        secondCar.color = "Blue";
+        secondCar.model = "Toyota Fortuner";
+        secondCar.year = 2023;
+
+        secondCar.startEngine();
+        secondCar.showDetails();
+    }
+}
---
+++ OOPS/Example_6_Inheritance/Dog.java
@@ -0,0 +1,8 @@
+package OOPS.Example_6_Inheritance;
+
+// Subclass
+class Dog extends Animal {
+    void bark() {
+        System.out.println(name + " is barking.");
+    }
+}
